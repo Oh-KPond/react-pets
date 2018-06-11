88c76e1bece12bb2ff69a8f4663c93c41b644f1f
Kate Pond <kate@thepondsedge.com>
2018-06-11 09:57:33 -0700

    creates static pet data 2

--- src/App.js
+++ src/App.js
@@ -6,13 +6,19 @@ const PET_LIST = [
     name: 'Bogary',
     age: 8,
     breed: 'Mixed',
-    about: 'Bogary was a mix off of the name Bogart'
+    about: 'Bogary was a mix off of the name Bogart.'
   },
   {
     name: 'Att',
     age: 20,
     breed: 'Irish Wolfhound',
     about: 'Att is very old'
+  },
+  {
+    name: 'Ziggy',
+    age: 50,
+    breed: 'Black Cat',
+    about: 'Ziggy was my mom\'s favorite cat.'
   }
 ]
 
