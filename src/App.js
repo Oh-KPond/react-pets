c986e61360bfd475b9717e1b18d98d56bf314dc7
Kate Pond <kate@thepondsedge.com>
2018-06-11 09:57:09 -0700

    creates static pet data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const PET_LIST = [
+  {
+    name: 'Bogary',
+    age: 8,
+    breed: 'Mixed',
+    about: 'Bogary was a mix off of the name Bogart'
+  },
+  {
+    name: 'Att',
+    age: 20,
+    breed: 'Irish Wolfhound',
+    about: 'Att is very old'
+  }
+]
+
 class App extends Component {
   render() {
     return (
