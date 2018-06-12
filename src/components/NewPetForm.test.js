42ada084965358d597ceafb5fa4056af111f2efc
Kate Pond <kate@thepondsedge.com>
2018-06-12 11:28:05 -0700

    creates test for event on NewPetForm

--- src/components/NewPetForm.test.js
+++ src/components/NewPetForm.test.js
@@ -1,18 +1,43 @@
 import React from 'react';
-import { mount } from 'enzyme';
-
+import { mount, shallow } from 'enzyme';
 import NewPetForm from './NewPetForm';
 
 describe('NewPetForm', () => {
   test('that it matches an existing snapshot', () => {
-  // Mount the component in the DOM
-  // Deep Rendering
+    // Mount the component in the DOM
+    // Deep Rendering
 
-  // Given
-  const wrapper = mount(<NewPetForm />);
-  // When and Then
-  expect(wrapper).toMatchSnapshot();
-  // Remove component from the DOM
-  wrapper.unmount();
+    // Given
+    const wrapper = mount(<NewPetForm
+      addPetCallback={() => {}}
+       />);
+    // When and Then
+    expect(wrapper).toMatchSnapshot();
+    // Remove component from the DOM
+    wrapper.unmount();
   });
+
+  test('when a user enters a name in a text field the field is updated', () => {
+    // Given
+    const wrapper = shallow(<NewPetForm
+        addPetCallback={() => {}}
+       />);
+
+    let nameField = wrapper.find('input[name="name"]');
+
+    // When
+    nameField.simulate('change', {
+      target: {
+        name: 'name',
+        value: 'Bob',
+      },
+    });
+
+    wrapper.update(); // Force the onChange preventDefault
+    nameField = wrapper.find('input[name="name"]');
+
+    // Then
+    expect(nameField.getElement().props.value).toEqual('Bob');
+
+  })
 });
