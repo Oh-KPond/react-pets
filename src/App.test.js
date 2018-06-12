afdf0de64c1b3eed7f9247da596c004099338d41
Kate Pond <kate@thepondsedge.com>
2018-06-12 11:03:03 -0700

    adds more snapshot test for App

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { mount, shallow } from 'enzyme';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// });
+
+describe ('App', () => {
+  // test('will match the last snapshot with deep rendering', () => {
+  //   const wrapper = mount(<App />);
+  //   expect(wrapper).toMatchSnapshot();
+  //   wrapper.unmount();
+  // });
+
+  test('that it renders App with shallow rendering', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper).toMatchSnapshot();
+    wrapper.unmount();
+  });
+})
