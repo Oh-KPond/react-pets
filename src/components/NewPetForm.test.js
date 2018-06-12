70dbc8e9f444ccadd818ae5ef192223749bfaf3d
Kate Pond <kate@thepondsedge.com>
2018-06-12 13:15:20 -0700

    adds a loop for field tests in form

--- src/components/NewPetForm.test.js
+++ src/components/NewPetForm.test.js
@@ -17,29 +17,29 @@ describe('NewPetForm', () => {
     wrapper.unmount();
   });
 
-  test('when a user enters a name in a text field the field is updated', () => {
-    // Given
-    const wrapper = shallow(<NewPetForm
-        addPetCallback={() => {}}
-       />);
-
-    let nameField = wrapper.find('input[name="name"]');
-
-    // When
-    nameField.simulate('change', {
-      target: {
-        name: 'name',
-        value: 'Bob',
-      },
-    });
-
-    wrapper.update(); // Force the onChange preventDefault
-    nameField = wrapper.find('input[name="name"]');
-
-    // Then
-    expect(nameField.getElement().props.value).toEqual('Bob');
-
-  })
+  // test('when a user enters a name in a text field the field is updated', () => {
+  //   // Given
+  //   const wrapper = shallow(<NewPetForm
+  //       addPetCallback={() => {}}
+  //      />);
+  //
+  //   let nameField = wrapper.find('input[name="name"]');
+  //
+  //   // When
+  //   nameField.simulate('change', {
+  //     target: {
+  //       name: 'name',
+  //       value: 'Bob',
+  //     },
+  //   });
+  //
+  //   wrapper.update(); // Force the onChange preventDefault
+  //   nameField = wrapper.find('input[name="name"]');
+  //
+  //   // Then
+  //   expect(nameField.getElement().props.value).toEqual('Bob');
+  //
+  // })
 
   test('When the user enters text it appears in that input field', () => {
     const wrapper = mount( <NewPetForm addPetCallback={() => {}} />);
@@ -64,15 +64,15 @@ describe('NewPetForm', () => {
     ];
 
     fieldValues.forEach(({field, value}) => {
-      let element = wrapper.find(`input[name="${field}"]`)
-      element.simulate('change', {target: {
+      let nameField = wrapper.find(`[name="${field}"]`)
+      nameField.simulate('change', {target: {
         name: field,
         value,
       }});
       wrapper.update();
-      element = wrapper.find(`input[name="${field}"]`);
-      expect(element.getElement().props.value).toEqual(value);
+      nameField = wrapper.find(`[name="${field}"]`);
+      expect(nameField.getElement().props.value).toEqual(value);
     });
   });
-  
+
 });
